9de83c50b81654d4aff98d1634cda735c56c5bc9
qcw <qcwthu@126.com>
2022-02-10 15:21:36 +0800

    modify ner

--- NER/runall_1.sh
+++ NER/runall_1.sh
@@ -1,21 +1,21 @@
 
 echo "Run conll to onto 2"
-cd ./NER/Conll2Onto_2
+cd ./Conll2Onto_2
 bash  NER_Conll2Onto_2.sh
 echo "Finish conll to onto 2"
 echo "----------------------"
-cd ../..
+cd ..
 
 echo "Run onto to conll 1"
-cd ./NER/Onto2Conll_1
+cd ./Onto2Conll_1
 bash  NER_Onto2Conll_1.sh
 echo "Finish onto to conll 1"
 echo "----------------------"
-cd ../..
+cd ..
 
 echo "Run conll to onto 1"
-cd ./NER/Conll2Onto_1
+cd ./Conll2Onto_1
 bash  NER_Conll2Onto_1.sh
 echo "Finish conll to onto 1"
 echo "----------------------"
-cd ../..
+cd ..
