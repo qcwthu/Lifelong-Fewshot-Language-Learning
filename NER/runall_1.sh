46087f4dd43a3287fce1877e929db124906d965f
qcw <qcwthu@126.com>
2022-02-10 10:36:21 +0800

    modify ner runall

--- NER/runall_1.sh
+++ NER/runall_1.sh
@@ -1,20 +1,21 @@
 
-
 echo "Run conll to onto 2"
-cd /data/qin/LFPT5_code/NER/Conll2Onto_2
+cd ./NER/Conll2Onto_2
 bash  NER_Conll2Onto_2.sh
 echo "Finish conll to onto 2"
 echo "----------------------"
+cd ../..
 
 echo "Run onto to conll 1"
-cd /data/qin/LFPT5_code/NER/Onto2Conll_1
+cd ./NER/Onto2Conll_1
 bash  NER_Onto2Conll_1.sh
 echo "Finish onto to conll 1"
 echo "----------------------"
-
+cd ../..
 
 echo "Run conll to onto 1"
-cd /data/qin/LFPT5_code/NER/Conll2Onto_1
+cd ./NER/Conll2Onto_1
 bash  NER_Conll2Onto_1.sh
 echo "Finish conll to onto 1"
 echo "----------------------"
+cd ../..
